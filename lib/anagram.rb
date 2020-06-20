425665c6c1465a81ce52394f9cdbb2a2ad27ccca
Jack Goode <jack.w.goode@icloud.com>
2020-06-20 13:33:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,9 @@ class AnagramDetector
   end
   
   def match(list_array)
-     list_array.select do |word|
+     @list_array = %(list_array)
+     
+     @list_array.select do |word|
       if word.split("").sort == @word.split("").sort
        end
     end
