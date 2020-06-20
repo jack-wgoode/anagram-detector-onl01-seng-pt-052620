6df7a1daa5aeb522bb7067c5c2334b441e3f8a49
Jack Goode <jack.w.goode@icloud.com>
2020-06-20 13:42:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
      @list_array = list_array
      
      @list_array.select do |scramble|
-      if scramble.split("").sort == self.word.split("").sort
+      if scramble.chars.split("").sort == self.word.chars.split("").sort
        end
     end
   end
