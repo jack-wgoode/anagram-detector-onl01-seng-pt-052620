259abf8c0983399665795e03314bed448626931f
Jack Goode <jack.w.goode@icloud.com>
2020-06-20 13:31:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class AnagramDetector
+  
+  attr_accessor :word
+  
+ def initialize(word) 
+  @word = word
+  end
+  
+  def match(list_array)
+     list_array.select do |word|
+      if word.split("").sort == @word.split("").sort
+       end
+    end
+  end
+  
+end
