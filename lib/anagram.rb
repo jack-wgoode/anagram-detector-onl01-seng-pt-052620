ebd8291906676f7948c39b603a989ad00394d915
Jack Goode <jack.w.goode@icloud.com>
2020-06-20 13:39:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,8 @@ class Anagram
   def match(list_array)
      @list_array = %(list_array)
      
-     self.list_array.select do |word|
-      if word.split("").sort == self.word.split("").sort
+     self.list_array.select do |scramble|
+      if scramble.split("").sort == self.word.split("").sort
        end
     end
   end
